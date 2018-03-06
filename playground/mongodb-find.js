96139e9a70e1cd9a2779712c288d81599adcd43c
Jurek <jurek2006@gmail.com>
2018-03-06 13:09:31 +0100

    Sekcja 7, łączenie z MongoDB, pobieranie, dodawanie i usuwanie danych

---
+++ playground/mongodb-find.js
@@ -0,0 +1,46 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    } 
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5a9e270820bfff341c886b10')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+        
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+        
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log('Todos', count);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count()
+    // .then((count) => {
+    //     console.log('Todos count', count);
+    // })
+    // .catch((err) => {
+    //     console.log('Unable to fetch todos', err);
+        
+    // });
+
+    db.collection('Users').find({
+        name: 'Jurek'
+    }).toArray()
+    .then(docs => {
+        console.log('Users:');
+        console.log((JSON.stringify(docs, undefined, 2)));
+    }).catch(err => {
+        console.log('Unable to fetch todos', err);
+    })
+
+    // db.close();
+});
