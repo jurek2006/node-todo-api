46223c5b7ef99dff94be80f37f5178425d374548
Jurek <jurek2006@gmail.com>
2018-03-07 14:37:07 +0100

    Dodanie pobierania pojedynczych todo GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 // server.js
+const {ObjectID} = require('mongodb');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -32,6 +34,22 @@ app.get('/todos', (req, res) => {
     }, err => {
         res.status(400).send(err);
     });
+});
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then(todo => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(err => res.status(400).send());
 });
 
 app.listen(3000, () => {
