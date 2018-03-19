c0385f14962ba7da122db317e3852da94e870c13
Jurek <jurek2006@gmail.com>
2018-03-19 09:51:00 +0100

    hasło

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 let connectPath, options;
 //Check if we are on Heroku
 if(process.env.PORT){
- connectPath = "mongodb://todouser@ds115729.mlab.com:15729/node-todo";
+ connectPath = "mongodb://todouser:Osiolek1925@ds115729.mlab.com:15729/node-todo";
  options= {
      auth: {
          user: 'todouser',
