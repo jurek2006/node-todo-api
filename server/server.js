d05af88c997a76874e43cd486ee591965b89d032
Jurek <jurek2006@gmail.com>
2018-03-19 16:04:03 +0100

    Dodana route PATCH /todos/:id do uaktualniania todo

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const {ObjectID} = require('mongodb');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -67,6 +68,31 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     }).catch(err => res.status(404).send());
 
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch(err => { res.status(400).send(); });
 });
 
 
