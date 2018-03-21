b07774f1315f2524bdea09574940124adec031c3
Jurek <jurek2006@gmail.com>
2018-03-21 14:54:58 +0100

    Hashowanie hasła użytkownika przed zapisem do bazy danych (problem - nie działa unikalność email przy tworzeniu użytkownika)

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-    id: 10
-}
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+const hashedPassword = '$2a$10$xaf3hd35jUX3fbuT/mNf1.ixmv/92AeWVvyeqT5rZdQsTDSrHAnQ6';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// const data = {
+//     id: 10
+// }
+
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // const message = 'I\'m user number 3';
